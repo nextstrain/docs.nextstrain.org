692000aadac9914528e5985c7c345638a8fca8b3
Thomas Sibley <tsibley@fredhutch.org>
2021-08-30 10:49:21 -0700

    Switch Intersphinx URL for the CLI from latest → stable
    We want stable doc to be the default in general, and the noted PR is
    long-since merged.

--- src/conf.py
+++ src/conf.py
@@ -73,7 +73,6 @@ html_theme_options = {
 intersphinx_mapping = {
     'augur': ('https://docs.nextstrain.org/projects/augur/en/stable', None),
     'auspice': ('https://docs.nextstrain.org/projects/auspice/en/stable', None),
-    # should be able to be change to stable after the next cli release (post https://github.com/nextstrain/cli/pull/102):
-    'cli': ('https://docs.nextstrain.org/projects/cli/en/latest/', None),
+    'cli': ('https://docs.nextstrain.org/projects/cli/en/stable/', None),
     'ncov': ('https://docs.nextstrain.org/projects/ncov/en/latest/', None)
 }
